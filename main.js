583d53b57e7e14d0b30af77f7b41866b8282dd6c
sergey120182 <sergey120182@mail.ru>
2017-05-16 02:28:43 +0300

    Create a new div with the date and time. Stylize. Add and clear the input field

--- main.js
+++ main.js
@@ -34,4 +34,26 @@ var infoStorage = JSON.parse(localStorage.getItem('list')) || [];
       $( "#opener" ).click(function() {
       $( "#dialog" ).dialog('instance');
      });
-     });                                                              
+     });
+         var newDiv = document.createElement("div");
+              newDiv.setAttribute("id","dialog");
+              var d = new Date();
+              var month = d.getMonth() + 1;
+              month = (month< 10) ? '0' + month : month;
+              var day =   d.getDate();
+              var year =  d.getFullYear();
+              var time1 = d.getUTCHours();
+              var time2 = d.getUTCMinutes();
+              time2 = (time2< 10) ? '0' + time2 : time2;
+              var time3 = d.getUTCSeconds();
+              time3 = (time3< 10) ? '0' + time3 : time3;
+              var date = day + '/' + month + '/' + year + '<br>' + time1 + ':' + time2 + ':' + time3;
+              newDiv.innerHTML = inp.val()+" "+ date;
+             $("#content").append(newDiv);
+             
+       inp.val('');});
+
+   $(function() {
+            $( "#opener" ).button();
+            $( "#opener" ).buttonset();
+         });                                                              
