c60f994607d81d0f9d93750a0c58a920f60f7653
sergey120182 <sergey120182@mail.ru>
2017-05-16 02:18:03 +0300

    A function that will listen and add a value key to the local store git commit -m

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ var infoStorage = JSON.parse(localStorage.getItem('list')) || [];
   function renderFromStorage () {
           $('#content').textContent = JSON.stringify(infoStorage);
                                 }
+ var addTask = function(){
+       var newVal = $("#text").val();
+       var newElement = {
+         key1 : newVal
+                        };
+              infoStorage.push(newVal);
+              localStorage.setItem('list', JSON.stringify(infoStorage));
+
+                               }                                
